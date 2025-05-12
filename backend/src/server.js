c8a1dd8a362c4426866b08e78ee0640c4e42989f
Shivam Gupta <shivamftp9696@gmail.com>
2025-05-12 18:57:20 +0530

    Fix CORS configuration in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -52,10 +52,10 @@ app.use(session({
     touchAfter: 24 * 3600 // time period in seconds
   }),
   cookie: {
-    secure: 'production',
+    secure: 'true',
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000, // 1 day
-    sameSite: 'false',
+    sameSite: 'none',
     path: '/'
   },
   name: 'bolt.sid' // Custom session cookie name
